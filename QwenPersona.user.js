6938131896d9fc65e34be89ad58ab71344364811
kev1nweng <kev1nweng@users.noreply.github.com>
2025-11-25 18:44:48 +0000

    deploy: ed67ea9e5125812d8569fcd77f7a050463187690

--- QwenPersona.user.js
+++ QwenPersona.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QwenPersona
 // @namespace    https://www.kev1nweng.space
-// @version      1764095199
+// @version      1764096288
 // @description  Qwen Chat Custom Persona
 // @author       小翁同学 (kev1nweng)
 // @license      AGPL-3.0
